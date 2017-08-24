54cf50007c4bfd4dd07e95878f38a7e456eae5ec
Simon Reymann <simon.reymann@gmail.com>
2017-08-24 14:47:11 +0200

    add missing index.js

---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { default as ProcessDefinitions } from './container/ProcessDefinitions/ProcessDefinitions';
+export { default as ProcessDefinition } from './container/ProcessDefinition/ProcessDefinition';
